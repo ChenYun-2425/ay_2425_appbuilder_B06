b33307f1956d1859b62a0f6726f3c66fceb3066b
Cheunghonchungklaus-2425 <240115749@stu.vtc.edu.hk>
2025-04-28 07:55:31 +0000

    ff

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ OPENID_PROVIDERS = [
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
 
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI = hktvmall-database.cidcw2rg51vi.us-east-1.rds.amazonaws.com
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+#SQLALCHEMY_DATABASE_URI = 'mysql://root:password@localhost/quickhowto'
 #SQLALCHEMY_DATABASE_URI = 'postgresql://scott:tiger@localhost:5432/myapp'
 #SQLALCHEMY_ECHO = True
 
